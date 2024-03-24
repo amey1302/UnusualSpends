aea591fec3ed529eb878ab0e1945e456441fb31c
amey1302 <ameykulkarni1322@gmail.com>
2024-03-24 16:37:33 +0530

    Add UnusualSpend analysis and Test cases

--- src/main/java/com/amaap/ttp/creditcard/domain/model/UnusualSpend.java
+++ src/main/java/com/amaap/ttp/creditcard/domain/model/UnusualSpend.java
@@ -3,6 +3,7 @@ package com.amaap.ttp.creditcard.domain.model;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -64,6 +65,38 @@ public class UnusualSpend {
     }
 
     public static Map<Category, Double> calculateUnusualSpend(List<Transaction> currentMonthTransactions, List<Transaction> previousMonthTransactions) {
-        return null;
+        Map<Category, Double> unusualSpends = new HashMap<>();
+        Map<Category, Double> currentMonthSpending = totalSpending(currentMonthTransactions);
+        Map<Category, Double> previousMonthSpending = totalSpending(previousMonthTransactions);
+
+        for (Category category : Category.values()) {
+            double currentMonthAmount = currentMonthSpending.getOrDefault(category, 0.0);
+            double previousMonthAmount = previousMonthSpending.getOrDefault(category, 0.0);
+
+            double percentageChange = ((currentMonthAmount - previousMonthAmount) / previousMonthAmount) * 100;
+            if (percentageChange >= 50.0)
+                unusualSpends.put(category, currentMonthAmount - previousMonthAmount);
+
+        }
+        return unusualSpends;
+    }
+
+    public static double totalUnusualAmountSpend(Map<Category, Double> unusualSpends) {
+        double totalUnusualAmount = 0.0;
+        for (double amount : unusualSpends.values()) {
+            totalUnusualAmount += amount;
+        }
+        return totalUnusualAmount;
+    }
+
+    public static Map<Category, Double> totalSpending(List<Transaction> transactions) {
+        Map<Category, Double> totalAmount = new HashMap<>();
+        for (Transaction transaction : transactions) {
+            Category category = transaction.getCategory();
+            Double amount = transaction.getAmount();
+            totalAmount.put(category, totalAmount.getOrDefault(category, 0.0) + amount);
+
+        }
+        return totalAmount;
     }
 }
--- src/test/java/com/amaap/ttp/creditcard/domain/model/UnusualSpendTest.java
+++ src/test/java/com/amaap/ttp/creditcard/domain/model/UnusualSpendTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,9 +24,10 @@ class UnusualSpendTest {
         //act
         double expectedTotal = 230.0 + 450.0 + 330.0;
         //assert
-        assertEquals(expectedTotal,UnusualSpend.getTotalAmountSpent());
+        assertEquals(expectedTotal, UnusualSpend.getTotalAmountSpent());
 
     }
+
     @Test
     void shouldAblToReturnCurrentMonthTransactions() throws InvalidTransactionException {
         //Arrange
@@ -41,9 +44,10 @@ class UnusualSpendTest {
         //System.out.println(currentMonthTransactions);
 
         //Assert
-        assertEquals(2,currentMonthTransactions.size());
+        assertEquals(2, currentMonthTransactions.size());
 
     }
+
     @Test
     void shouldAblToReturnCurrentPreviousMonthTransactions() throws InvalidTransactionException {
         //Arrange
@@ -61,10 +65,40 @@ class UnusualSpendTest {
         //System.out.println(previousMonthTransactions);
 
         //Assert
-        assertEquals(2,previousMonthTransactions.size());
+        assertEquals(2, previousMonthTransactions.size());
 
     }
 
+    @Test
+    void shouldAbleToGiveUnusualSpendWithCategoryAndAmount() throws InvalidTransactionException {
+        //Arrange
+        CreditCard creditCard = new CreditCard(1);
+        UnusualSpend unusualSpend = new UnusualSpend();
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.add(Transaction.create(1, LocalDate.of(2024, 03, 13), 730.0, Category.Groceries));
+        transactions.add(Transaction.create(2, LocalDate.of(2024, 02, 14), 250.0, Category.Travel));
+        transactions.add(Transaction.create(3, LocalDate.of(2024, 03, 13), 500.0, Category.Travel));
+        transactions.add(Transaction.create(4, LocalDate.of(2024, 02, 13), 330.0, Category.Groceries));
+        creditCard.setTransactions(transactions);
+
+        //Act
+        List<Transaction> currentMonthTransactions = UnusualSpend.currentMonthTransactions(creditCard.getTransactions());
+        List<Transaction> prevMonthTransactions = UnusualSpend.previousMonthTransactions(creditCard.getTransactions());
+
+
+        Map<Category, Double> actual = UnusualSpend.calculateUnusualSpend(currentMonthTransactions, prevMonthTransactions);
+
+        // System.out.println(actual);
+        // Assert
+        Map<Category, Double> expected = new HashMap<>();
+        expected.put(Category.Groceries, 400.0);
+        expected.put(Category.Travel, 250.0);
+        assertEquals(expected, actual);
+    }
+
+    
 
 
 }
+
+
