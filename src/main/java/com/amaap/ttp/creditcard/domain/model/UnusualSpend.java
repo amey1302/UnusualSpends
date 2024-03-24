7e5c80aad0d0d9d1a0598a096435595906ea30d6
amey1302 <ameykulkarni1322@gmail.com>
2024-03-24 16:20:24 +0530

    Add Current Month Transaction with test case

--- src/main/java/com/amaap/ttp/creditcard/domain/model/UnusualSpend.java
+++ src/main/java/com/amaap/ttp/creditcard/domain/model/UnusualSpend.java
@@ -1,5 +1,8 @@
 package com.amaap.ttp.creditcard.domain.model;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -17,7 +20,22 @@ public class UnusualSpend {
 
 
     public static List<Transaction> currentMonthTransactions(List<Transaction> transactions) {
-        return null;
+
+        List<Transaction> currentMonthTransactions = new ArrayList<>();
+        LocalDate date = LocalDate.now();
+        Month currentMonth = date.getMonth();
+
+        int currentYear = date.getYear();
+
+        for (Transaction transaction : transactions) {
+            LocalDate transactionDate = transaction.getDate();
+            Month transactionMonth = transactionDate.getMonth();
+            int transactionYear = transactionDate.getYear();
+            if (transactionMonth == currentMonth && transactionYear == currentYear) {
+                currentMonthTransactions.add(transaction);
+            }
+        }
+        return currentMonthTransactions;
     }
 
     public static List<Transaction> previousMonthTransactions(List<Transaction> transactions) {
--- src/test/java/com/amaap/ttp/creditcard/domain/model/UnusualSpendTest.java
+++ src/test/java/com/amaap/ttp/creditcard/domain/model/UnusualSpendTest.java
@@ -25,5 +25,24 @@ class UnusualSpendTest {
         assertEquals(expectedTotal,UnusualSpend.getTotalAmountSpent());
 
     }
+    @Test
+    void shouldAblToReturnCurrentMonthTransactions() throws InvalidTransactionException {
+        //Arrange
+        CreditCard creditCard = new CreditCard(1);
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.add(Transaction.create(1, LocalDate.of(2024, 03, 13), 230.0, Category.Groceries));
+        transactions.add(Transaction.create(2, LocalDate.of(2024, 03, 14), 450.0, Category.Groceries));
+        transactions.add(Transaction.create(3, LocalDate.of(2024, 02, 13), 330.0, Category.Groceries));
+        creditCard.setTransactions(transactions);
+
+        //Act
+        List<Transaction> currentMonthTransactions = UnusualSpend.currentMonthTransactions(transactions);
+
+        //System.out.println(currentMonthTransactions);
+
+        //Assert
+        assertEquals(2,currentMonthTransactions.size());
+
+    }
 
 }
