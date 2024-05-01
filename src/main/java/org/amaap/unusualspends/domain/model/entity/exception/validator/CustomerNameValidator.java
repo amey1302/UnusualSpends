9392138b40c77168911a4c28aa0cff35629df1c9
amey1302 <ameykulkarni1322@gmail.com>
2024-05-01 11:47:41 +0530

    Add CustomerNameValidator

---
+++ src/main/java/org/amaap/unusualspends/domain/model/entity/exception/validator/CustomerNameValidator.java
@@ -0,0 +1,11 @@
+package org.amaap.unusualspends.domain.model.entity.exception.validator;
+
+public class CustomerNameValidator {
+    public static boolean isInValidCustomerName(String customerName){
+        return !isCustomerNameValid(customerName);
+    }
+    public static boolean isCustomerNameValid(String name) {
+        return name != null && !name.isEmpty() && name.matches("^([A-Za-z]{4,} [A-Za-z]{3,})$");
+    }
+
+}
