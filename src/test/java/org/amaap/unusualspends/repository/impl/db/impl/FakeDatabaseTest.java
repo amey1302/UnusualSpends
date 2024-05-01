5cb155f2de302840c444ba560deb5cf3c9b79153
amey1302 <ameykulkarni1322@gmail.com>
2024-05-01 14:48:18 +0530

    Add test cases for fakedatabase

---
+++ src/test/java/org/amaap/unusualspends/repository/impl/db/impl/FakeDatabaseTest.java
@@ -0,0 +1,31 @@
+package org.amaap.unusualspends.repository.impl.db.impl;
+
+import org.amaap.unusualspends.domain.model.entity.Customer;
+import org.amaap.unusualspends.domain.model.entity.exception.customerexception.InvalidCustomerDataException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FakeDatabaseTest {
+   private FakeDatabase fakeDatabase;
+   @BeforeEach
+    void setUp(){
+       fakeDatabase =  new FakeDatabase();
+
+   }
+   @Test
+    void shouldBeABleToInsertCustomerDataIntoCustomerTable() throws InvalidCustomerDataException {
+       // arrange
+       int id = 1;
+       String name = "Baburao Apte";
+       String email = "baburao.apte@gmail.com";
+       Customer expected = Customer.create(id,name,email);
+
+       // act
+        Customer actual = fakeDatabase.insertIntoCustomerTable(expected);
+
+       // assert
+       assertEquals(expected,actual);
+   }
+}
