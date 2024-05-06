7f7a980fd6a6c830755406e7a4a4265d180608ca
amey1302 <ameykulkarni1322@gmail.com>
2024-05-06 22:51:38 +0530

    Add exception class for CreditCard

---
+++ src/main/java/org/amaap/unusualspends/domain/model/entity/exception/creditcardexception/InvalidCardIdException.java
@@ -0,0 +1,7 @@
+package org.amaap.unusualspends.domain.model.entity.exception.creditcardexception;
+
+public class InvalidCardIdException extends Exception {
+    public InvalidCardIdException(String message) {
+        super(message);
+    }
+}
